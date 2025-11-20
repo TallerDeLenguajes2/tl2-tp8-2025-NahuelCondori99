d97a86d4d53d8a2e2c88cb756f570d3be426c4d3
NahuelCondori99 <nahuelcondori62t@gmail.com>
2025-11-20 00:14:32 -0300

    Termino la parte de Details.cshtml y todo anda joya

--- obj/Debug/net8.0/tl2-tp8-2025-NahuelCondori99.AssemblyInfo.cs
+++ obj/Debug/net8.0/tl2-tp8-2025-NahuelCondori99.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl2-tp8-2025-NahuelCondori99")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9da4a367e69e05eab00b5c61a7fcdea213e5b1d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e78a3fd2517fb6e01335cb4632c43a75ac4ed9db")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl2-tp8-2025-NahuelCondori99")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl2-tp8-2025-NahuelCondori99")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
