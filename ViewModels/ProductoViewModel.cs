ea588f2112bb61fc73cb1f0cb64d12272580b911
NahuelCondori99 <nahuelcondori62t@gmail.com>
2025-11-26 23:55:52 -0300

    Creo mis ViewModels

---
+++ ViewModels/ProductoViewModel.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace tl2_tp8_2025_NahuelCondori99.ViewModels
+{
+    public class ProductoViewModel
+    {
+        public int IdProducto {get; set;}
+
+        [Display(Name = "Descripcion del producto")]
+        [StringLength(250, ErrorMessage = "La descripcion no puede superar los 250 caracteres")]
+        public string Descripcion{get; set;}
+
+        [Display(Name = "Precio unitario")]
+        [Required(ErrorMessage = "El precio es obligatorio")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser mayor a 0.")]
+        public decimal Precio {get; set;}
+    }
+}
--- obj/Debug/net8.0/tl2-tp8-2025-NahuelCondori99.AssemblyInfo.cs
+++ obj/Debug/net8.0/tl2-tp8-2025-NahuelCondori99.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl2-tp8-2025-NahuelCondori99")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71d16087f98fe9ccd23703d70d780a0f01c764e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d70377d4d97bf603ebb6ee97b2fa74ecf2b4078")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl2-tp8-2025-NahuelCondori99")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl2-tp8-2025-NahuelCondori99")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
